ebe76bce34132210e51e3152586ad0bca4b90797
Kristi Miller <kristi.ann.miller@gmail.com>
2020-07-09 20:24:41 -0600

    Create showFormView function

--- src/main.js
+++ src/main.js
@@ -1,10 +1,11 @@
 // query selector variables go here 👇
-
 var title = document.querySelector('.poster-title');
 var imageUrl = document.querySelector('.poster-img');
 var quote = document.querySelector('.poster-quote');
 var randomButton = document.querySelector('.show-random');
-
+var makeYourOwnPosterButton = document.querySelector('.show-form');
+var mainPosterView = document.querySelector('.main-poster');
+var createPosterFormView = document.querySelector('.poster-form');
 
 // we've provided you with some data to work with 👇
 var images = [
@@ -109,21 +110,31 @@ var savedPosters = [];
 
 var currentPoster;
 
+
 // event listeners go here 👇
+
 randomButton.addEventListener('click', displayCurrentPoster);
+makeYourOwnPosterButton.addEventListener('click', showFormView);
 
 
 // functions and event handlers go here 👇
 
 
+
 // (we've provided one for you to get you started)!
 function getRandomIndex(array) {
   return Math.floor(Math.random() * array.length);
-}
+};
+
 function displayCurrentPoster() {
   title.innerText = titles[getRandomIndex(titles)];
   imageUrl.src = images[getRandomIndex(images)];
   quote.innerText = quotes[getRandomIndex(quotes)];
 };
 
-displayCurrentPoster();
+window.onload = displayCurrentPoster();
+
+function showFormView() {
+  mainPosterView.classList.add('hidden');
+  createPosterFormView.classList.remove('hidden');
+};
